e55843e6d793603b7a6d85d2990832c6267b133c
Simon Jenni <simujenni@gmail.com>
2017-08-29 15:33:50 +0200

    Fixed path of checkpoints to work on both cluster and local machine

--- train_imagenet_inverter.py
+++ train_imagenet_inverter.py
@@ -2,6 +2,8 @@ from Preprocessor import Preprocessor
 from train.AlexNetInverter_Trainer import CNetTrainer
 from datasets.ImageNet import ImageNet
 from models.AlexNetInverter import AlexNetInverter
+from constants import LOG_DIR
+import os
 
 target_shape = [128, 128, 3]
 model = AlexNetInverter(batch_size=32, target_shape=target_shape, num_layers=5, layer_id=1)
@@ -9,6 +11,6 @@ data = ImageNet()
 preprocessor = Preprocessor(target_shape=target_shape)
 trainer = CNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=10, tag='inv_tv',
                       lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=0.00003)
-ckpt = '/Data/Logs/CNet/imagenet_SDNet_res1_default_baseline_finetune_conv_5/model.ckpt-324174'
+ckpt = os.path.join(LOG_DIR, 'imagenet_SDNet_res1_default_baseline_finetune_conv_5/model.ckpt-324174')
 
 trainer.train_inverter(ckpt)
